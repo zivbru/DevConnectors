1d9893ca6084eccc3688e2b5abe44a0312fcec67
ziv <zivbru123@gmail.com>
2020-06-04 10:40:28 +0300

    Added post component, manage likes and unlikes to posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -47,7 +47,6 @@ router.post(
 router.get('/', auth, async (req, res) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
-
     res.json(posts);
   } catch (err) {
     console.error(err.message);
